017a18b7002890698ea1b849eada3f27a51b4fb6
LodynskiMarine <marine.lodynski1@gmail.com>
2024-02-05 06:55:36 +0100

    Variables pour les différents projets de sophiebluel avec des créations de balises...

--- FrontEnd/javascript/projects.js
+++ FrontEnd/javascript/projects.js
@@ -2,6 +2,20 @@
 const reponse = await fetch("package-lock.json");
 const project = await reponse.json();
 
+//Variables projets
+const gallery = document.querySelector(".gallery");
+const figure = document.createElement("figure");
+gallery.appendChild(figure);
+const img = document.createElement("img");
+const figcaption = document.createElement("figcaption");
+figure.appendChild(img);
+figure.appendChild(figcaption);
+
+
+
+
+
+
 
 
 // Filtres travaux
