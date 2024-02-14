6a44d2ec46c89db5804e52cf51fec0d754641b89
LodynskiMarine <marine.lodynski1@gmail.com>
2024-02-14 20:14:05 +0100

    Mentorat modifications et explications filtre : un seul fetch

--- FrontEnd/javascript/projects.js
+++ FrontEnd/javascript/projects.js
@@ -1,33 +1,6 @@
 // Récupération travaux
-//async function RecupTravaux() {
-    //const reponse = await fetch("http://localhost:5678/api/works");
-    //const projects = await reponse.json();
-    //console.log(JSON.stringify(projects));
-//}
-//RecupTravaux();
-
-//async function AjoutTravaux() {
-	//for(const user of projects){
-		//let figure = document.createElement("figure");
-		//const imageElement = document.createElement("img");
-		//const figcaption = document.createElement("figcaption");
-		//imageElement.src=user.imageUrl;
-		//figcaption.innerText=user.title;
-		//gallery.appendChild(figure);
-		//figure.appendChild(imageElement);
-		//figure.appendChild(figcaption);
-	//}
-	//RecupTravaux();
-//}
-//AjoutTravaux();
-
 const gallery = document.querySelector(".gallery");
 async function RecupTravaux() {
-    //const reponse = await fetch("http://localhost:5678/api/works");
-    //const projects = await reponse.json();
-    //console.log(JSON.stringify(projects));
-
-
 	fetch("http://localhost:5678/api/works", {
 		method: "GET",
 		headers: { "Content-Type": "application/json" },
@@ -51,47 +24,9 @@ async function RecupTravaux() {
 			gallery.appendChild(figure);
 			figure.appendChild(imageElement);
 			figure.appendChild(figcaption);
-		
-	
 		}
 
 	
-	});
-
-
-
-
-}
-RecupTravaux();
-
-
-async function FiltrerTravaux() {
-	fetch("http://localhost:5678/api/works", {
-		method: "GET",
-		headers: { "Content-Type": "application/json" },
-	})
-
-	.then((response) => {	
-		return response.json();
-	})
-
-	.then ((users) => {
-		//console.log(users)
-
-		//Ajout des travaux dans la galerie
-		for(const user of users) {
-			let figure = document.createElement("figure");
-			const imageElement = document.createElement("img");
-			const figcaption = document.createElement("figcaption");
-			imageElement.src=user.imageUrl;
-			imageElement.alt=user.title;
-			figcaption.innerText=user.title;
-			gallery.appendChild(figure);
-			figure.appendChild(imageElement);
-			figure.appendChild(figcaption);
-		
-	
-		}
 		let listeBTNFilters = document.querySelectorAll(".filters");
 		for(let index = 0; index < listeBTNFilters.length; index++) {
 			listeBTNFilters[index].addEventListener("click", function(event) {
@@ -99,14 +34,6 @@ async function FiltrerTravaux() {
 				FilterButton();
 			})
 		}
-
-	});
-    
-		
-	
-	
-		
-
 
 		function FilterButton() {
 			//const categoryall = users.filter(user => user.category.name === "Tous");
@@ -143,8 +70,17 @@ async function FiltrerTravaux() {
 		};
 
 
+
+
+	});
+
+
+
+
 }
-FiltrerTravaux();
+RecupTravaux();
+
+
 
 
 
