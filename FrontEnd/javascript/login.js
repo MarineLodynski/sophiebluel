2d4dfd533aaee4c5e9cfda5fe2cfb80a3e1cf896
LodynskiMarine <marine.lodynski1@gmail.com>
2024-02-14 17:42:49 +0100

    Début login.js ainsi que des modifications concernant le login form

---
+++ FrontEnd/javascript/login.js
@@ -0,0 +1,14 @@
+//Page de connexion
+fetch("http://localhost:5678/api/users/login", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+	})
+
+	.then((response) => {	
+		return response.json();
+	})
+
+	.then ((users) => {
+		//console.log(users)
+    }
+)    
--- FrontEnd/javascript/projects.js
+++ FrontEnd/javascript/projects.js
@@ -1,6 +1,4 @@
 // Récupération travaux
-
-
 //async function RecupTravaux() {
     //const reponse = await fetch("http://localhost:5678/api/works");
     //const projects = await reponse.json();
@@ -24,91 +22,129 @@
 //AjoutTravaux();
 
 const gallery = document.querySelector(".gallery");
-fetch("http://localhost:5678/api/works", {
-	method: "GET",
-	headers: { "Content-Type": "application/json" },
-})
+async function RecupTravaux() {
+    //const reponse = await fetch("http://localhost:5678/api/works");
+    //const projects = await reponse.json();
+    //console.log(JSON.stringify(projects));
 
-.then((response) => {
-	return response.json();
-})
 
-.then ((users) => {
-	console.log(users)
+	fetch("http://localhost:5678/api/works", {
+		method: "GET",
+		headers: { "Content-Type": "application/json" },
+	})
 
-	//Ajout des travaux dans la galerie
-	for(const user of users) {
-		let figure = document.createElement("figure");
-		const imageElement = document.createElement("img");
-		const figcaption = document.createElement("figcaption");
-		imageElement.src=user.imageUrl;
-		imageElement.alt=user.title;
-		figcaption.innerText=user.title;
-		gallery.appendChild(figure);
-		figure.appendChild(imageElement);
-		figure.appendChild(figcaption);
+	.then((response) => {	
+		return response.json();
+	})
+
+	.then ((users) => {
+		//console.log(users)
+
+		//Ajout des travaux dans la galerie
+		for(const user of users) {
+			let figure = document.createElement("figure");
+			const imageElement = document.createElement("img");
+			const figcaption = document.createElement("figcaption");
+			imageElement.src=user.imageUrl;
+			imageElement.alt=user.title;
+			figcaption.innerText=user.title;
+			gallery.appendChild(figure);
+			figure.appendChild(imageElement);
+			figure.appendChild(figcaption);
 		
 	
-	}
+		}
 
-	//Filtres pour les travaux
 	
-	
+	});
 
-	function FilterButton() {
-		//const categoryall = users.filter(user => user.category.name === "Tous");
-		//console.log(categoryall);
-		//const categoryobjects = users.filter(user => user.category.name === "Objets");
-		//console.log(categoryobjects);
-		//const categoryappartments = users.filter(user => user.category.name === "Appartements");
-		//console.log(categoryappartments);
-		//const categoryhr = users.filter(user => user.category.name === "Hotels & restaurants");
-		//console.log(categoryhr);
 
-		const categories = users.map(user => user.categoryId);
-		console.log(categories);
+
+
+}
+RecupTravaux();
+
+
+async function FiltrerTravaux() {
+	fetch("http://localhost:5678/api/works", {
+		method: "GET",
+		headers: { "Content-Type": "application/json" },
+	})
+
+	.then((response) => {	
+		return response.json();
+	})
+
+	.then ((users) => {
+		//console.log(users)
+
+		//Ajout des travaux dans la galerie
+		for(const user of users) {
+			let figure = document.createElement("figure");
+			const imageElement = document.createElement("img");
+			const figcaption = document.createElement("figcaption");
+			imageElement.src=user.imageUrl;
+			imageElement.alt=user.title;
+			figcaption.innerText=user.title;
+			gallery.appendChild(figure);
+			figure.appendChild(imageElement);
+			figure.appendChild(figcaption);
 		
-
-		//switch(categoryId){
-			//case 1:
-				//users.categoryId === 1
-				//break;
-			//case 2:
-				//users.categoryId === 2
-				//break;
-			//case 3:
-				//users.categoryId === 3
-				//break;
-			//default:
-				//;
-		//}
-
-		//const filteredImages = figure.filter(function(filter) {
-			//return users.categoryId === 2; // Remplacez 1 par la categoryId que vous souhaitez filtrer
-		//});
-		//console.log(filteredImages);
-	};
 	
-	
-
-	let listeBTNFilters = document.querySelectorAll(".filters");
+		}
+		let listeBTNFilters = document.querySelectorAll(".filters");
 		for(let index = 0; index < listeBTNFilters.length; index++) {
 			listeBTNFilters[index].addEventListener("click", function(event) {
 				console.log('Vous avez cliqué !');
 				FilterButton();
 			})
 		}
-});
 
-
-
+	});
+    
+		
 	
 	
+		
 
 
+		function FilterButton() {
+			//const categoryall = users.filter(user => user.category.name === "Tous");
+			//console.log(categoryall);
+			//const categoryobjects = users.filter(user => user.category.name === "Objets");
+			//console.log(categoryobjects);
+			//const categoryappartments = users.filter(user => user.category.name === "Appartements");
+			//console.log(categoryappartments);
+			//const categoryhr = users.filter(user => user.category.name === "Hotels & restaurants");
+			//console.log(categoryhr);
+	
+			const categories = users.map(user => user.categoryId);
+			console.log(categories);
+			
+	
+			//switch(categoryId){
+				//case 1:
+					//users.categoryId === 1
+					//break;
+				//case 2:
+					//users.categoryId === 2
+					//break;
+				//case 3:
+					//users.categoryId === 3
+					//break;
+				//default:
+					//;
+			//}
+	
+			//const filteredImages = figure.filter(function(filter) {
+				//return users.categoryId === 2; // Remplacez 1 par la categoryId que vous souhaitez filtrer
+			//});
+			//console.log(filteredImages);
+		};
 
 
-
+}
+FiltrerTravaux();
 
 
 
